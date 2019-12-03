80e70597b9bf031eff57cccd14843227db20b527
Elena Volkova <flika_v@mail.ru>
2019-12-03 16:03:47 +0300

    del

---
+++ test/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class Main
+{
+
+
+    public static void main(String[] args)
+    {
+        String[]arr = {"java", "C"};
+
+
+//        for (int i=0; i < arr.length; i++)
+//        {
+//            arr[i] += "class";
+//            System.out.println(arr[i]);
+//        }
+
+//        for (String e: arr )
+//        {
+//            e += 11;
+//            System.out.println(e);
+//
+//        }
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
+
+
+
